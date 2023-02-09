8e56506b0dcdcdf1e59c3a0f5000e9effa2a66fa
ishit16 <ishitgarg1231@gmail.com>
2023-02-09 19:48:46 +0530

    FEAT: LoginForm Created, Style Needs to be Improved

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import "././customcss/landing.css"
+
+export const LandingPage = () => {
+    return (
+        <>
+            <div className="bg-purple-landing-page h-screen">
+                <div className="flex justify-center items-center">
+                    <p className="text-pink-600 text-5xl landing-page-heading-font font-bold">CodeGames</p>
+                </div>
+            </div>
+        </>
+    );
+}
