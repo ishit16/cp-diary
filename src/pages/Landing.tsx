59b3421b090376b6d842eccc2f4450887b84c11a
ishit16 <ishitgarg1231@gmail.com>
2023-02-10 20:20:29 +0530

    IMPROV: Landing Page Design Improved

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,23 @@
 import "././customcss/landing.css";
-import image from "../assets/computer.png";
+import image from "../assets/human-coding.png";
+import peepsImage from "../assets/peep-crowd.svg";
 import { LoginForm } from "../components/login/LoginForm";
+import titleImage from "../assets/codeforces-diary.svg";
 export const LandingPage = () => {
   return (
     <>
       <div className="bg-purple-landing-page h-screen">
-        <div className="grid grid-flow-col auto-cols-auto">
-          <div className="h-screen grid content-center background-image"></div>
+        <div className="grid grid-cols-2">
+          <div
+            className="h-screen flex flex-col justify-between background-image"
+            style={{ width: "90vh" }}
+          >
+            <img style={{ width: "90vh" }} src={titleImage}></img>
+            <img style={{ width: "90vh" }} src={image}></img>
+            <img style={{ width: "90vh" }} src={peepsImage}></img>
+          </div>
           <div className="h-screen grid place-items-center">
-            <div className="h-3/4 w-3/4 bg-slate-500 bg-opacity-25 rounded-xl">
+            <div className="h-3/4 w-11/12 bg-slate-500 bg-opacity-25 rounded-xl">
               <div className="text-center mx-auto">
                 <p className="text-white py-16  font-bold text-4xl">
                   Login Form
