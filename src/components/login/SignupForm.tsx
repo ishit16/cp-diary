b13b9f323abd117f51948f5ad97e5a04afd1ffd3
ishit16 <ishitgarg1231@gmail.com>
2023-02-26 23:17:08 +0530

    FEAT: Signup form integrated with backend, working

--- src/components/login/SignupForm.tsx
+++ src/components/login/SignupForm.tsx
@@ -1,9 +1,77 @@
+import axios from "../../api/axios";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = "/register";
+
 export const SignupForm = () => {
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+  }, [user]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd]);
+
+  const handleSubmit = async (e) => {
+    console.log("Hii");
+    e.preventDefault();
+    const v1 = USER_REGEX.test(user);
+    const v2 = PWD_REGEX.test(pwd);
+    if (!v1 || !v2) {
+      setErrMsg("Invalid Entry");
+      return;
+    }
+    try {
+      console.log("Ishit");
+      const response = await axios.post(
+        REGISTER_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(response?.data);
+      console.log(response?.accessToken);
+      console.log(JSON.stringify(response));
+      setSuccess(true);
+      setUser("");
+      setPwd("");
+      setMatchPwd("");
+    } catch (err) {
+      //@ts-ignore
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+        // @ts-ignore
+      } else if (err.response?.status === 409) {
+        setErrMsg("Username Taken");
+      } else {
+        setErrMsg("Registration Failed");
+      }
+      //@ts-ignore
+    }
   };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -11,6 +79,10 @@ export const SignupForm = () => {
           <input
             id="username"
             type="text"
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
             className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
             placeholder="Username"
           />
@@ -20,6 +92,9 @@ export const SignupForm = () => {
           <input
             id="password"
             type="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
             className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
             placeholder="Password"
           />
@@ -27,22 +102,28 @@ export const SignupForm = () => {
 
         <div className="mb-6">
           <input
-            id="password"
+            id="confirm_pwd"
             type="password"
+            onChange={(e) => setMatchPwd(e.target.value)}
+            value={matchPwd}
+            required
             className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
             placeholder="Confirm Password"
           />
         </div>
-
-        <Link
-          type="submit"
+        <button
           className="inline-block text-center px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-          to={"/dashboard"}
+          disabled={!validName || !validPwd || !validMatch ? true : false}
         >
+          {/* <Link
+            type="submit"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            to={"/dashboard"}
+          > */}
           Sign Up
-        </Link>
+          {/* </Link> */}
+        </button>
 
         <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
           <p className="text-center font-semibold mx-4 mb-0 text-white">OR</p>
