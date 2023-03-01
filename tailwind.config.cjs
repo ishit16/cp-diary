31534c05b0fb828d954f2e14fcb522fab5170d21
ishit16 <ishitgarg1231@gmail.com>
2023-03-01 23:28:47 +0530

    FEAT: Added new fonts and colors for diary

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "purple-landing-page": "#16213E",
         "navbar-dashboard-color": "#0A2647",
         "lite-white": "rgba(255, 255, 255, 0.17)",
-      }
+        "category-heading": "hsl(333deg, 100%, 52%)",
+      },
+      fontFamily: {
+        "category-heading": ['"Lexend"', "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
