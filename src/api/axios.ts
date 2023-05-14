d50138ed2395b4485319a72885d1fe0863aacda5
ishit16 <ishitgarg1231@gmail.com>
2023-05-14 16:31:20 +0530

    BUILD: ReactDevTools Disabled

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://cp-diary-api.onrender.com';
 
 
 export default axios.create({
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
